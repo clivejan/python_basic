95a5e74c37c7cebd983c86e06b840ac009b5775d
Clive <clive.jan@gmail.com>
2020-01-07 12:23:42 -0500

    Update: [Project] Password Locker - Handle command line arguments

--- projects/pw.py
+++ projects/pw.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 # pw.py Am insecure password locker program.
 
+import sys
+
 PASSWORD = {'email': 'emailpassword',
 			'blog': 'blogpassword',
 			'luggage': '12345'}
+
+
+if len(sys.argv) < 2:
+	print("Usage: python3 pw.py [accounnt] - copy account passowrd")
+	sys.exit()
+
+account = sys.argv[1]
