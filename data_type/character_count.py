5fc84b72365f6586fd38bea7fd65c41d4883c93b
Clive <clive.jan@gmail.com>
2020-01-07 08:34:34 -0500

    Add: counting characters in a string

---
+++ data_type/character_count.py
@@ -0,0 +1,8 @@
+message = "I hope I can find a job in Canada before running out my money."
+
+count = {}
+for character in message:
+	count.setdefault(character, 0)
+	count[character] += 1
+
+print(count)
