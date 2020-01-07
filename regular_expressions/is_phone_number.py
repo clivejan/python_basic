55dfc89d4c96c2b0d199aa17847048584bffa81c
Clive <clive.jan@gmail.com>
2020-01-07 14:01:55 -0500

    Update: find a phone number without regex in a string

--- regular_expressions/is_phone_number.py
+++ regular_expressions/is_phone_number.py
@@ -22,5 +22,12 @@ def is_phone_number(text):
 
 	return True
 
-print(f"415-555-4242 is a phone number: {is_phone_number('415-555-4242')}")
-print(f"Moshi moshi is a phone number: {is_phone_number('Moshi moshi')}")
+message = "Call me at 415-555-1011 tomorrow. 415-555-9999 is my office."
+for i in range(len(message)):
+	chunk = message[i:i+12]
+	if is_phone_number(chunk):
+		print(f"Phone number found: {chunk}")
+print("Done")
+
+#print(f"415-555-4242 is a phone number: {is_phone_number('415-555-4242')}")
+#print(f"Moshi moshi is a phone number: {is_phone_number('Moshi moshi')}")
