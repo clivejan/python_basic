c8dd4e34f4b6a1b44c2703b0ed798074f89efd44
Clive <clive.jan@gmail.com>
2020-01-07 12:27:54 -0500

    Update: [Project] Password Locker - Copy the right password

--- projects/pw.py
+++ projects/pw.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # pw.py Am insecure password locker program.
 
-import sys
+import sys, pyperclip
 
 PASSWORD = {'email': 'emailpassword',
 			'blog': 'blogpassword',
@@ -12,4 +12,11 @@ if len(sys.argv) < 2:
 	print("Usage: python3 pw.py [accounnt] - copy account passowrd")
 	sys.exit()
 
+# First command line arg is the account name
 account = sys.argv[1]
+if account in PASSWORD.keys():
+	pyperclip.copy(PASSWORD[account])
+	print(f"Password for {account} copied to clipboard.")
+else:
+	print(f"There is no account named {account}.")
+
