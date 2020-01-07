a5cf5643c1e205f143c118ac3b1bf6f2165c3643
Clive <clive.jan@gmail.com>
2020-01-07 12:38:17 -0500

    Update: [Project] Adding Bullets to Wiki Markup - Copy and paste from the clipboard

--- projects/bullet_point_adder.py
+++ projects/bullet_point_adder.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 # bullet_point_adder.py - Adds Wikipedoa bullet points to the start
 # of each lin of text on the clipboard
+
+import pyperclip
+
+text = pyperclip.paste
+
+# TODO: Separate lines and add stars
+
+pyperclip.paste()
