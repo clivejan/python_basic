7ec7be507084cb706150e71a29e2fedf777a8374
Clive <clive.jan@gmail.com>
2020-01-08 15:58:07 -0500

    Add: practice some operations on file level

---
+++ fileio/file_operation.py
@@ -0,0 +1,26 @@
+import shutil, os
+
+# Copy a single file
+#os.chdir('/tmp/py_test_dir')
+#shutil.copy('hello.txt', '/tmp')
+#shutil.copy('hello.txt', '/tmp/haha.txt')
+
+# Copy an entire folder
+#shutil.copytree('/Users/clive/CliveSpace/github/Python',
+#	'/tmp/py_test_dir/Python')
+
+# Move a file into a folder
+#shutil.move('/tmp/haha.txt', '/tmp/py_test_dir/Python')
+
+# Rename a file
+#shutil.move('/tmp/py_test_dir/Python/haha.txt',
+#	'/tmp/py_test_dir/Python/hello.txt')
+
+# Delete a file
+#os.unlink('/tmp/py_test_dir/Python/hello.txt')
+
+# Delete an empty folder
+#os.rmdir('/tmp/py_test_dir/test')
+
+# Delete an non empty folder
+#lshutil.rmtree('/tmp/py_test_dir/Python')
