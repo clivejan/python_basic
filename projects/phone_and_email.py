a7ca65303e58e5767d5a6cdf8e578659e5cc96f8
Clive <clive.jan@gmail.com>
2020-01-08 09:21:12 -0500

    Fix: [Project] Phone Number and Email Address Extrator - address several typos and errors

--- projects/phone_and_email.py
+++ projects/phone_and_email.py
@@ -4,40 +4,40 @@
 import re, pyperclip
 
 # Create phome number regex
-phone_regex = (r'''
+phone_regex = re.compile(r'''(
 	(\d{3}|\(\d{3}\))?				# area code
 	(\s|-|\.)?						# separator
 	(\d{3})							# first 3 digits
 	(\s|-|\.)?						# separator
 	(\d{4})							# last 4 digits
 	(\s*(ext|x|.ext)\s*(\d{2,5}))?	# extension
-	''', re.VERBOSE)
+	)''', re.VERBOSE)
 
 # Create email regex
-email_regex = (r'''
+email_regex = re.compile(r'''(
 	[a-zA-Z0-9._%+-]+				# username
 	@								# @ symbol
 	[a-zA-Z0-9.-]+					# doamin name
 	(\.[a-zA-Z]{2,4})				# dot-something
-	''', re.VERBOSE)
+	)''', re.VERBOSE)
 
 # Findn matches in clipboard text
 text = pyperclip.paste()
 matches = []
 
 for groups in phone_regex.findall(text):
-	phone_number = '-'.join(groups[1], groups(3), groups(5))
-	if groups(8):
-		phone_number = f"{phone_number} x{groups(8)}"
+	phone_number = '-'.join([groups[1], groups[3], groups[5]])
+	if len(groups[8]):
+		phone_number = f"{phone_number} x{groups[8]}"
 	matches.append(phone_number)
 
 for groups in email_regex.findall(text):
-	matches.append(groups(0))
+	matches.append(groups[0])
 
 # Copy results to the clipboard
 if len(matches) > 0:
 	pyperclip.copy('\n'.join(matches))
 	print("Copied to clipboard:")
-	prinnt(f"{'\n'.join(matches)}")
+	print('\n'.join(matches))
 else:
 	print("No phone numbers or email addresses found.")
