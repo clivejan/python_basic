27b842a3276dd4744337cfdbcf9b7e7689442e79
Clive <clive.jan@gmail.com>
2020-01-08 08:39:05 -0500

    Update: [Project] Phone Number and Email Address Extrator - create regex for phone numbers

--- projects/phone_and_email.py
+++ projects/phone_and_email.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 # phone_and_email.py - Finds phone numbers and email addresses on the clipboard
+
+import re, pyperclip
+
+# Create phome number regex
+phone_regex = (r'''
+	(\d{3}|\(\d{3}\))?				# area code
+	(\s|-|\.)?						# separator
+	(\d{3})							# first 3 digits
+	(\s|-|\.)?						# separator
+	(\d{4})							# last 4 digits
+	(\s*(ext|x|.ext)\s*(\d{2,5}))?	# extension
+	''', re.VERBOSE)
