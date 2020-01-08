83235e8bdd6d48a00a7c3eb3a73caa7f314e5464
Clive <clive.jan@gmail.com>
2020-01-07 20:16:55 -0500

    Add: regex practice - grouping with parentheses

---
+++ regular_expressions/is_phone_number_group.py
@@ -0,0 +1,8 @@
+import re
+
+phone_num_regex = re.compile(r'(\d\d\d)-(\d\d\d\-\d\d\d\d)')
+mo = phone_num_regex.search('My number is 415-555-4242.')
+print(mo.group())
+print(mo.group(0))
+print(mo.group(1))
+print(mo.group(2))
