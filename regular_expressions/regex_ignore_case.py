71ce12a545af9b4dd7a7b3b8bcc835da0c45b3b7
Clive <clive.jan@gmail.com>
2020-01-07 21:25:15 -0500

    Add: case-insensitive matching by re.I

---
+++ regular_expressions/regex_ignore_case.py
@@ -0,0 +1,6 @@
+import re
+
+robocop = re.compile(r'robocop', re.I)
+
+print(robocop.search('RoboCop is part man, part machine, all cop').group())
+print(robocop.search('ROBOCOP protects the innocent.').group())
