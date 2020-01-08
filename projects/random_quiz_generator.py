98e47bdadf34ad25b24d10e5c1b2918eb8a3d70c
Clive <clive.jan@gmail.com>
2020-01-08 13:31:03 -0500

    Update: [Project] Generating Random Quiz Files - store the quiz data in dictionary

--- projects/random_quiz_generator.py
+++ projects/random_quiz_generator.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env python3
 # random_quiz_generator.py - Creates quizzes with questions and answers
 # in random order, along with the answer key.
+
+import random
+
+# The quiz data. Keys are states and values are their capotals.
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
+'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 
+'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 
+'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 
+'Michigan':'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
+'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln',
+'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 
+'NewMexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':'Nashville',
+'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':'Montpelier',
+'Virginia': 'Richmond', 'Washington': 'Olympia','WestVirginia': 'Charleston',
+'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+
+# Generate 35 quiz files.
+for quiz_num in range(35):
+	# TODO: Create the quzi and answer key files.
+
+	# TODO: Write out the header for the quiz.
+
+	# TODO: Shuffle the order of the states.
+
+	# TODO: loop through all 50 states, making a queston for each.
