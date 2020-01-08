d6670f79b871e14e67c69ac1c7453ef82001197b
Clive <clive.jan@gmail.com>
2020-01-08 08:50:22 -0500

    Update: [Project] Phone Number and Email Address Extrator - create regex for email addresses

--- projects/phone_and_email.py
+++ projects/phone_and_email.py
@@ -12,3 +12,11 @@ phone_regex = (r'''
 	(\d{4})							# last 4 digits
 	(\s*(ext|x|.ext)\s*(\d{2,5}))?	# extension
 	''', re.VERBOSE)
+
+# Create email regex
+email_regex = (r'''
+	[a-zA-Z0-9._%+-]+				# username
+	@								# @ symbol
+	[a-zA-Z0-9.-]+					# doamin name
+	(\.[a-zA-Z]{2,4})				# dot-something
+	''', re.VERBOSE)
