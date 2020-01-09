e7a1bb7ddf4f4f1b221ce0a8bcefb0d240be8418
Clive <clive.jan@gmail.com>
2020-01-08 19:13:40 -0500

    Add: [Project] Rename file with US dates to EU dates

---
+++ projects/rename_dates.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# rename_dates.py - Rename filenames with US MM-DD-YYYY date format
+# to EU DD-MM-YYYY
+
+# TODO" Create a regex that meatches files with the US date format.
+
+# TODO: Loop over the files in the specified directory.
+
+# TODO: Skip files without a date.
+
+# TODO: Get the different parts of the filename.
+
+# TODO: Form the EU-style filename
+
+# TODO Rename the files
