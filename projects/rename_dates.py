76f02dd831f74f4b5230de63bc221614535151e0
Clive <clive.jan@gmail.com>
2020-01-08 19:17:39 -0500

    Update: [Project] Rename file with US dates to EU dates - create a regex for US dates

--- projects/rename_dates.py
+++ projects/rename_dates.py
@@ -2,7 +2,16 @@
 # rename_dates.py - Rename filenames with US MM-DD-YYYY date format
 # to EU DD-MM-YYYY
 
-# TODO" Create a regex that meatches files with the US date format.
+import shutil, os, re
+
+# Create a regex that meatches files with the US date format.
+date_regex = re.compile(r'''(
+	^(.*?)					# all text before the date
+	((0|1)?\d)-				# one or two digits for the month
+	((0|1|2|3)?\d)-			# one or four digits for the month
+	((19|20)\d\d)			# four digits for the year
+	(.*?)$					# all text after the date
+	)''', re.VERBOSE)
 
 # TODO: Loop over the files in the specified directory.
 
