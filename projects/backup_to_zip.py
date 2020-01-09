9c18638083a505db916097290df1e4183490938a
Clive <clive.jan@gmail.com>
2020-01-08 19:55:01 -0500

    Update: [Project] Backing Up a Folder into a ZIP File - figure out the ZIP filename

--- projects/backup_to_zip.py
+++ projects/backup_to_zip.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
-# backup_tp_zip.py - Copies an entire folder and its contens into
+# backup_to_zip.py - Copies an entire folder and its contens into
 # a ZIP file whose filename increments.
+
+import zipfile, os
+
+# Backup the entire content of "folder" into a ZIP file
+def backup_to_zip(folder):
+	folder = os.path.abspath(folder)
+
+	# Figure out the filename this code should use based on 
+	# what files already exists.
+	number = 1
+	while True:
+		zip_filename = f"{os.path.basename(folder)}_{number}.zip"
+		if not os.path.exists(zip_filename):
+			break
+		number += 1
+
+	# TODO: Create the ZIP file
+
+	# TODO: Walk the entire folder tree and compress the file in each folder.
+
+	print('Done.')
+
+backup_to_zip('/Users/clive/CliveSpace/github/Python')
