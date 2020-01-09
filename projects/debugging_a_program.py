773de644a87792b872e38017dcac34c1691ccc87
Clive <clive.jan@gmail.com>
2020-01-09 09:43:36 -0500

    Update: [Project] Debugging Coin Toss - done

--- projects/debugging_a_program.py
+++ projects/debugging_a_program.py
@@ -1,17 +1,27 @@
-import random
+#!/usr/bin/env python3
+
+import random, logging
+
+logging.basicConfig(level=logging.DEBUG, filename='/tmp/python_error.log',\
+	format=' %(asctime)s - %(levelname)s - %(message)s')
 
 guess = ''
 while guess not in ('heads', 'tails'):
 	guess = input('Guessthe coin toss! Enter heads or tails: ')
 
-toss = random.randint(0, 1)
+toss = ['heads', 'tails']
+random.shuffle(toss)
+logging.debug(f"toss_list: {toss}")
 
-if toss == guess:
+logging.debug(f'toss: {toss[0]}, guess: {guess}')
+
+if toss[0] == guess:
 	print('You got it!')
 else:
-	print('Nopt! Guess again!')
-	guesss = input()
-	if toss == guess:
+	guess = input('Nopt! Guess again! ')
+	logging.debug(f'toss: {toss[0]}, guess: {guess}')
+
+	if toss[0] == guess:
 		print('You got it!')
 	else:
 		print('Nopt! You are really bad at this game.')
