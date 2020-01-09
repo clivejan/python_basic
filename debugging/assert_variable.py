42387d30e6f9d7349103a4b58476949dbcb49d91
Clive <clive.jan@gmail.com>
2020-01-09 08:28:49 -0500

    Add: using assert statment to do sanity check

---
+++ debugging/assert_variable.py
@@ -0,0 +1,10 @@
+# Assertion is used for programmer errors and 
+# should not use try except to handle it.
+
+# Status well
+job_title = 'DevOps'
+assert job_title == "DevOps", "Tansform from SE to DevOps"
+
+# Status wrong
+job_title = 'Systems Engineer'
+assert job_title == "DevOps", "Tansform from SE to DevOps"
